78b49842cdbb43e74e4d139bc07b6aa2a965ca3c
MarcioFCMartins <marciomartinsred@gmail.com>
2022-05-25 19:07:25 +0100

    Combined filter and merge functionality in one script
    The filter diagnostics and filtering process is now done in the
    merge-sequences.R script to simplify workflow. Started planning
    the code to diagnose the several steps performed, as well as the
    code to assign taxonomy.

--- merge-sequences.R
+++ merge-sequences.R
@@ -4,7 +4,8 @@
 # Date: 2022-05-24
 
 # Install and load required packages --------------------------------------
-packages <- c("BiocManager", "optparse", "dada2", "stringr", "ggplot2", "dplyr")
+packages <- c("BiocManager", "optparse", "dada2", "stringr", 
+              "ggplot2", "dplyr", "tidyr")
 
 installed_packages <- packages %in% rownames(installed.packages())
 # Try to install packages from CRAN
@@ -26,6 +27,7 @@ if (any(installed_packages == FALSE)) {
 
 invisible(lapply(packages, library, character.only = TRUE))
 set.seed(123)
+
 # Read arguments ----------------------------------------------------------
 option_list <- list(
     make_option(
@@ -43,9 +45,7 @@ option_list <- list(
 opt_parser <- OptionParser(option_list = option_list)
 opts <- parse_args(opt_parser)
 
-
 # Prepare list of samples -------------------------------------------------
-
 files <- data.frame(
     "path" = list.files(
         path = opts$`data-directory`,
@@ -62,6 +62,7 @@ files$forward <- str_detect(files$filename, "_R1_")
 files$sample <- str_extract(files$filename, "[^_]*")
 # Prepare paths for output file
 files$output_file <- file.path(opts$output, files$filename)
+files <- files[order(files$sample), ]
 
 # Check if there's 2 files for every sample
 # This check is rudimentary and doesn't see if every sample name has a file
@@ -72,55 +73,167 @@ if (length(files$forward[files$forward]) != length(files$forward[files$forward])
 fwd_samples <- files[files$forward, ]
 rev_samples <- files[!files$forward, ]
 
-fwd_samples_vec <- fwd_samples$path
-names(fwd_samples_vec) <- fwd_samples$sample
+# Preview forward read quality and determine cutoff  ----------------------
+message("Preparing preview of forward read quality. Please hold.")
+message("If you are using this program over ssh, make sure X11 fowarding is on!")
+message("The plot might take a while to be shown.")
 
-rev_samples_vec <- rev_samples$path
-names(rev_samples_vec) <- rev_samples$sample
+# Create quality plot
+qual_plot <- plotQualityProfile(fwd_samples$path, aggregate = TRUE) +
+    labs(
+        title = "Forward read sequence quality",
+        subtitle = "Select cutoff point"
+    )
 
+# Calculate average quality for each sequence length
+qual_stats <- qual_plot$data |>
+    group_by(Cycle) |>
+    summarise(avg = weighted.mean(Score, Count))
+
+# Display quality plot
+plot_temp <- tempfile("plot", fileext = ".png") # temporary file for plot
+ggsave(plot_temp, qual_plot)
+browseURL(plot_temp)
+
+# Wait for user to input the desired length
+message("Please input the foward read cutoff point.")
+message(
+    paste0(
+        "The average nucleotide quality dips below 30 for the ",
+        "first time in position ",
+        qual_stats$Cycle[min(which(qual_stats$avg < 30))],
+        "."
+    )
+)
+
+fwd_cut <- scan("stdin", integer(), n = 1)
+
+
+# Preview reverse read quality and determine cutoff -----------------------
+message("Preparing preview of reverse read quality. Please hold.")
+message("If you are using this program over ssh, make sure X11 fowarding is on!")
+message("The plot might take a while to be shown.")
+
+# Create quality plot
+qual_plot <- plotQualityProfile(rev_samples$path, aggregate = TRUE) +
+    labs(
+        title = "Reverse read sequence quality",
+        subtitle = "Select cutoff point"
+    )
+
+# Calculate average quality for each sequence length
+qual_stats <- qual_plot$data |>
+    group_by(Cycle) |>
+    summarise(avg = weighted.mean(Score, Count))
+
+# Display quality plot
+plot_temp2 <- tempfile("plot2", fileext = ".png") # temporary file for plot
+ggsave(plot_temp2, qual_plot)
+browseURL(plot_temp2)
+
+message("Please input the reverse read cutoff point.")
+message(
+    paste0(
+        "The average nucleotide quality dips below 30 for the ",
+        "first time in position ",
+        qual_stats$Cycle[min(which(qual_stats$avg < 30))],
+        "."
+    )
+)
+
+rev_cut <- scan("stdin", integer(), n = 1)
+
+
+# Perform sequence filtering ----------------------------------------------
+# Have to use matchID - https://github.com/benjjneb/dada2/issues/737
+
+message("Filtering sequences. Please hold")
+filtered_output <- dada2::filterAndTrim(
+    fwd = fwd_samples$path,
+    filt = fwd_samples$output_file,
+    rev = rev_samples$path,
+    filt.rev = rev_samples$output_file,
+    truncLen = c(fwd_cut, rev_cut),
+    matchIDs = TRUE,
+    multithread = TRUE,
+    verbose = TRUE
+)
+
+write.csv(
+    filtered_output,
+    file.path(opts$output, "0000-filtering-stats.csv")
+)
 
 # Perform sample inference ------------------------------------------------
+fwd_samples_vec <- fwd_samples$output_file
+names(fwd_samples_vec) <- fwd_samples$sample
+
+rev_samples_vec <- rev_samples$output_file
+names(rev_samples_vec) <- rev_samples$sample
+
+message("Performing sequence inference.")
 # Learn forward error rates
 fwd_error <- learnErrors(
     fwd_samples_vec, 
     nbases=1e8, 
     multithread=TRUE, 
-    randomize = TRUE)
+    randomize = TRUE
+)
 
 # Learn reverse error rates
 rev_error <- learnErrors(
     rev_samples_vec, 
     nbases=1e8, 
     multithread=TRUE,
-    randomize = TRUE)
+    randomize = TRUE
+)
 
+
+# Dereplicate amplicon sequences to improve computational speed of inference
+fwd_derep <- derepFastq(fwd_samples_vec)
+rev_derep <- derepFastq(rev_samples_vec)
+
+# Perform inference
 fwd_dada <- dada(
-    derep = fwd_samples_vec, 
+    derep = fwd_derep, 
     err   = fwd_error,
     pool  = "pseudo",
-    multithread = TRUE)
+    multithread = TRUE
+)
 
 rev_dada <- dada(
-    derep = rev_samples_vec, 
+    derep = rev_derep, 
     err   = rev_error,
     pool  = "pseudo",
-    multithread = TRUE)
+    multithread = TRUE
+)
 
 # Merge sequences ---------------------------------------------------------
-
+message("Merging paired-end sequences")
 merged <- mergePairs(
     fwd_dada,
-    fwd_samples_vec, 
+    fwd_derep, 
     rev_dada,
-    rev_samples_vec,
+    rev_derep,
     verbose=TRUE
 )
 
 
 # Sequence table ----------------------------------------------------------
 seqtab <- makeSequenceTable(merged)
-dim(seqtab)
-table(nchar(getSequences(seqtab)))
+
+
+
+# Assign taxonomy script starts here --------------------------------------
+###########################################################################
 
 seqtab <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE)
 
+
+# TEST, MOVE LATER --------------------------------------------------------
+
+tax <- assignTaxonomy(
+    seqtab,
+    "./Onedrive/700-in-progress-projects/706 - Arrabida restored seagrass meadows/2_data/edna-sequencing-processing/silva_132.18s.99_rep_set.dada2.fa.gz",
+    multithread = TRUE,
+    verbose = TRUE)
---
+++ test-diagnostics.R
@@ -0,0 +1,42 @@
+dim(seqtab)
+table(nchar(getSequences(seqtab)))
+
+
+
+getN <- function(x) sum(getUniques(x))
+track <- cbind(
+    filtered_output, 
+    sapply(fwd_dada, getN), 
+    sapply(rev_dada, getN), 
+    sapply(merged, getN), 
+    rowSums(seqtab))
+# If processing a single sample, remove the sapply calls: e.g. replace sapply(dadaFs, getN) with getN(dadaFs)
+colnames(track) <- c("input", "filtered", "denoisedF", "denoisedR", "merged", "nonchim")
+rownames(track) <- fwd_samples$sample
+View(track)
+
+track <- as.data.frame(track)
+track$sample <- rownames(track)
+
+track_long <- track |>
+    mutate(
+        depth = as.numeric(str_replace(
+            str_extract(sample, "-[\\d]*"),
+            "-", 
+            ""
+        )
+        )) |>
+    pivot_longer(
+        cols = input:nonchim
+    ) |>
+    mutate(
+        name = factor(name, levels = names(track))
+    )
+
+ggplot(track_long) +
+    geom_line(
+        aes(x = name,
+            y = log10(value),
+            group = sample)
+    ) +
+    theme_minimal()
